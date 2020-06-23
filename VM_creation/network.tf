ecb4d9d8a11cef5afba0f2a6240f21bab24a95c8
raju30 <raju30.wls@gmail.com>
2020-06-23 12:18:34 +0530

    Added VM terrafrom templates

--- VM_creation/network.tf
+++ VM_creation/network.tf
@@ -16,7 +16,7 @@ resource "google_compute_subnetwork" "public_subnet_1" {
 # allow internal icmp (disable for better security)
 resource "google_compute_firewall" "allow-internal" {
   name    = "${var.app_name}-fw-allow-internal"
-  network = "${google_compute_network.vpc.name}"
+  network = google_compute_network.vpc.name
   allow {
     protocol = "icmp"
   }
