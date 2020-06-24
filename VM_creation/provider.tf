2a1972b5f02767c73dc8eb94f646f29695ed47cd
pradeepv30 <raju30.wls@gmail.com>
2020-06-24 14:13:56 +0530

    Added files

--- VM_creation/provider.tf
+++ VM_creation/provider.tf
@@ -6,7 +6,7 @@ terraform {
 
 provider "google" {
   project = var.project_name
-  #credentials = file(var.gcp_auth_file)
+  credentials = file(var.gcp_auth_file)
   region  = var.gcp_region_1
   zone    = var.gcp_zone_1
 }
